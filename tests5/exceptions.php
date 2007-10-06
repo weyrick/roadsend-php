7dd6195ac057f1a77f70a2c2846a922f0c9a5b36
Shannon Weyrick <weyrick@mozek.us>
2007-10-06 19:45:27 +0000

    missing test

---
+++ tests5/exceptions.php
@@ -0,0 +1,44 @@
+<?php
+
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Division by zero.');
+    }
+    else return 1/$x;
+}
+
+function inverse2($x) {
+    if (!$x) {
+        throw new Other2('Division by zero.');
+    }
+    else return 1/$x;
+}
+
+class Other2 extends Exception {
+    var $foo = 'excellent';
+}
+
+try {
+    echo inverse(5) . "\n";
+    echo inverse(0) . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+} catch (Other $e) {
+    echo 'Caught other exception';
+} catch (Other2 $e) {
+    echo "Caught other exception 2";
+}
+
+// Continue execution
+echo 'Hello World';
+
+try {
+    echo inverse2(0) . "\n";
+} catch (Other $e) {
+    echo 'Caught other exception';
+} catch (Other2 $e) {
+    echo "Caught other exception 2";
+}
+
+
+?>
