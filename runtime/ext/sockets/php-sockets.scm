8ae86ea86ea7e44051ce8bc9fdd114b3b6c2ba8b
Shannon Weyrick <weyrick@mozek.us>
2008-02-08 17:47:43 +0000

    working basic socket implementation

--- runtime/ext/sockets/php-sockets.scm
+++ runtime/ext/sockets/php-sockets.scm
@@ -24,36 +24,144 @@
    (export
     (init-php-sockets-lib)
     ;
+    AF_UNIX
+    AF_INET
+    AF_INET6
+    SOCK_STREAM
+    SOCK_DGRAM
+    SOCK_RAW
+    SOCK_SEQPACKET 
+    SOCK_RDM
+    MSG_OOB 
+    MSG_WAITALL 
+    MSG_PEEK 
+    MSG_DONTROUTE 
+    MSG_EOR 
+    MSG_EOF 
+    SO_DEBUG 
+    SO_REUSEADDR 
+    SO_KEEPALIVE 
+    SO_DONTROUTE 
+    SO_LINGER 
+    SO_BROADCAST 
+    SO_OOBINLINE 
+    SO_SNDBUF 
+    SO_RCVBUF 
+    SO_SNDLOWAT 
+    SO_RCVLOWAT
+    SO_SNDTIMEO 
+    SO_RCVTIMEO
+    SO_TYPE 
+    SO_ERROR
+    PHP_NORMAL_READ
+    PHP_BINARY_READ 
+    SOL_TCP
+    SOL_UDP
+    SOL_SOCKET
+    ;
+;    (socket_bind sock address port)
     (socket_close sock)
+    (socket_connect sock address cport)    
     (socket_create domain type protocol)
+    (socket_read sock len readtype)
+    (socket_write sock buffer len)
     ;
     ))
 
 (define (init-php-sockets-lib) 1)
 
+(defresource php-socket "Socket"
+   bsocket
+   connected?
+   last-error-code
+   last-error-str)
+
+(define *socket-counter* 0)
+(define (make-finalized-socket)
+   (when (> *socket-counter* 255) ; ARB
+      (gc-force-finalization (lambda () (<= *socket-counter* 255))))
+   (let ((new-sock (php-socket-resource #f #f 0 "")))
+      (set! *socket-counter* (+fx *socket-counter* 1))
+      (register-finalizer! new-sock (lambda (sock)
+				       (unless (socket-down? sock)
+					  (socket-shutdown (php-socket-bsocket sock))
+					  (set! *socket-counter* (- *socket-counter* 1)))))
+      new-sock))
+
 ; register the extension
-(register-extension "sockets" "1.0.0" "php-socket")
+(register-extension "sockets" "1.0.0" "php-sockets")
 
 ; socket_accept - Accepts a connection on a socket
 ; socket_bind - Binds a name to a socket
+;(defbuiltin (socket_bind sock address (port 'unset))
+;   (echo "in socket_bind"))
+
 ; socket_clear_error - Clears the error on the socket or the last error code
 ; socket_close - Closes a socket resource
 (defbuiltin (socket_close sock)
-   (echo "in socket_close"))
+   (if (php-socket? sock)
+       (if (or (not (php-socket-connected? sock))
+	       (and (socket? (php-socket-bsocket sock))
+		    (socket-down? (php-socket-bsocket sock))))
+	   #f
+	   (begin
+	      (socket-shutdown (php-socket-bsocket sock))
+	      #t))
+       #f))
 
 ; socket_connect - Initiates a connection on a socket
+(defbuiltin (socket_connect sock address cport)
+   (if (php-socket? sock)
+       (if (php-socket-connected? sock)
+	   #f
+	   (with-handler (lambda (e)
+			    ; XXX no error codes? non 0 for now
+			    (php-socket-last-error-code-set! sock 1)
+			    (php-socket-last-error-str-set! sock (&io-error-msg e))	
+			    #f)
+			 (begin
+			    (php-socket-bsocket-set! sock (make-client-socket (mkstr address)
+									      (mkfixnum cport)))
+			    (php-socket-connected?-set! sock #t)
+			    #t)))
+       #f))
+
 ; socket_create_listen - Opens a socket on port to accept connections
 ; socket_create_pair - Creates a pair of indistinguishable sockets and stores them in an array
+
 ; socket_create - Create a socket (endpoint for communication)
 (defbuiltin (socket_create domain type protocol)
-   (echo "in socket_create"))
-
+   ;
+   ; XXX bigloo only supports IP4, stream, and tcp
+   ;
+   (unless (and (php-= domain AF_INET)
+		(php-= type SOCK_STREAM)
+		(php-= protocol SOL_TCP))
+      (php-error "socket_create currently only supports AF_INET, SOCK_STREAM, SOL_TCP"))
+   ; ok
+   (make-finalized-socket))
+   
 ; socket_get_option - Gets socket options for the socket
 ; socket_getpeername - Queries the remote side of the given socket which may either result in host/port or in a Unix filesystem path, dependent on its type
 ; socket_getsockname - Queries the local side of the given socket which may either result in host/port or in a Unix filesystem path, dependent on its type
 ; socket_last_error - Returns the last error on the socket
 ; socket_listen - Listens for a connection on a socket
+
 ; socket_read - Reads a maximum of length bytes from a socket
+(defbuiltin (socket_read sock len (readtype PHP_NORMAL_READ))
+   (if (and (php-socket? sock) (php-socket-connected? sock))
+       (let* ((read-proc (cond ((php-= readtype PHP_NORMAL_READ) 'binary)
+			       ((php-= readtype PHP_BINARY_READ) 'text)
+			       (else 'binary)))
+	      (read-len (maxfx 1 (mkfixnum len)))
+	      (inbuf  (if (eqv? read-proc 'binary)
+			  (read-chars read-len (socket-input (php-socket-bsocket sock)))
+			  (read-line (socket-input (php-socket-bsocket sock))))))
+	  (if (eof-object? inbuf)
+	      ""
+	      inbuf))
+       #f))
+	  
 ; socket_recv - Receives data from a connected socket
 ; socket_recvfrom - Receives data from a socket whether or not it is connection-oriented
 ; socket_select - Runs the select() system call on the given arrays of sockets with a specified timeout
@@ -64,5 +172,61 @@
 ; socket_set_option - Sets socket options for the socket
 ; socket_shutdown - Shuts down a socket for receiving, sending, or both
 ; socket_strerror - Return a string describing a socket error
+
 ; socket_write - Write to a socket
+(defbuiltin (socket_write sock buffer (len 'unset))
+   (if (and (php-socket? sock) (php-socket-connected? sock))
+       (let* ((buf (mkstr buffer))
+	      (buf-size (if (eqv? len 'unset)
+			    (string-length buf)
+			    (minfx (string-length buf) (mkfixnum len))))
+	      (real-buf (if (<fx buf-size (string-length buf))
+			    (substring buf 0 buf-size)
+			    buf)))
+	  (display real-buf (socket-output (php-socket-bsocket sock)))
+	  (flush-output-port (socket-output (php-socket-bsocket sock)))
+	  ; XXX we're assuming they were all written
+	  buf-size
+	  )
+       #f))
+
+(defconstant AF_UNIX  0)
+(defconstant AF_INET  1)
+(defconstant AF_INET6 2)
+
+(defconstant SOCK_STREAM 0)
+(defconstant SOCK_DGRAM 1)
+(defconstant SOCK_RAW 2)
+(defconstant SOCK_SEQPACKET 3)
+(defconstant SOCK_RDM 4)
+
+(defconstant MSG_OOB 0)
+(defconstant MSG_WAITALL 1)
+(defconstant MSG_PEEK 2)
+(defconstant MSG_DONTROUTE 3)
+(defconstant MSG_EOR 4)
+(defconstant MSG_EOF 5)
+
+(defconstant SO_DEBUG 0)
+(defconstant SO_REUSEADDR 1)
+(defconstant SO_KEEPALIVE 2)
+(defconstant SO_DONTROUTE 3)
+(defconstant SO_LINGER 4)
+(defconstant SO_BROADCAST 5)
+(defconstant SO_OOBINLINE 6)
+(defconstant SO_SNDBUF 7)
+(defconstant SO_RCVBUF 8)
+(defconstant SO_SNDLOWAT 9)
+(defconstant SO_RCVLOWAT 10)
+(defconstant SO_SNDTIMEO 11)
+(defconstant SO_RCVTIMEO 12)
+(defconstant SO_TYPE 13) 
+(defconstant SO_ERROR 14)
+   
+(defconstant PHP_NORMAL_READ 0)
+(defconstant PHP_BINARY_READ 1)
+   
+(defconstant SOL_TCP 0)
+(defconstant SOL_UDP 1)
+(defconstant SOL_SOCKET 2)
 
---
+++ runtime/ext/sockets/tests/http-get.php
@@ -0,0 +1,44 @@
+<?php
+
+echo "<h2>TCP/IP Connection</h2>\n";
+
+/* Get the port for the WWW service. */
+$service_port = getservbyname('www', 'tcp');
+
+/* Get the IP address for the target host. */
+$address = gethostbyname('www.roadsend.com');
+
+/* Create a TCP/IP socket. */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+echo "Attempting to connect to '$address' on port '$service_port'...";
+$result = socket_connect($socket, $address, $service_port);
+if ($result === false) {
+    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+$in = "GET /test.txt HTTP/1.1\r\n";
+$in .= "Host: www.roadsend.com\r\n";
+$in .= "Connection: Close\r\n\r\n";
+$out = '';
+
+echo "Sending HTTP HEAD request...";
+socket_write($socket, $in, strlen($in));
+echo "OK.\n";
+
+echo "Reading response:\n\n";
+while ($out = socket_read($socket, 2048)) {
+    echo $out;
+}
+
+echo "Closing socket...";
+socket_close($socket);
+echo "OK.\n\n";
+?>
