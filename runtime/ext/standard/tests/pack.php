0a82fcd6f1c81477ecb5f09aafe9ee0d2498a609
Shannon Weyrick <weyrick@mozek.us>
2008-02-23 16:39:58 +0000

    make the pack test output clearer. this appears to highlight not only problems in pack, but in our onums and/or sprintf

--- runtime/ext/standard/tests/pack.php
+++ runtime/ext/standard/tests/pack.php
@@ -1,5 +1,40 @@
 <?php
 
+function hexdump($a) {
+    $s = '';
+    for ($i=0; $i<strlen($a); $i++) {
+        $s .= sprintf("[%02x] ",ord($a{$i}));
+    }
+    return $s."\n";
+}
+
+
+function tp($fmt, $val) {
+    static $index=0;
+    echo "[$index]: orig  : ".sprintf("%d | 0x%02x\n",$val,$val);
+    $v = pack($fmt, $val);
+    echo "[$index]: pack  : ".hexdump($v);
+    $v = unpack($fmt, $v);
+    echo "[$index]: unpack: ".sprintf("%d | 0x%02x\n",$v[1],$v[1]);
+    echo "-----------------------\n";
+    $index++;
+}
+
+tp("N", 65534);
+tp("N", 0);
+tp("N", 2147483650);
+tp("N", 4294967296);
+tp("N", -2147483648);
+tp("N", -30000);
+
+tp("V", 65534);
+tp("V", 0);
+tp("V", 2147483650);
+tp("V", 4294967296);
+tp("V", -2147483648);
+
+
+/*
 print_r(unpack("A", pack("A", "hello world")));
 print_r(unpack("A*", pack("A*", "hello world")));
 echo '"'.pack("A9", "hello").'"';
@@ -25,25 +60,12 @@ print_r(unpack("L", pack("L", 2147483650)));
 print_r(unpack("L", pack("L", 4294967295)));
 print_r(unpack("L", pack("L", -2147483648)));
 
-print_r(unpack("N", pack("N", 65534)));
-print_r(unpack("N", pack("N", 0)));
-print_r(unpack("N", pack("N", 2147483650)));
-print_r(unpack("N", pack("N", 4294967296)));
-print_r(unpack("N", pack("N", -2147483648)));
-print_r(unpack("N", pack("N", -30000)));
-
 print_r(unpack("S", pack("S", 65534)));
 print_r(unpack("S", pack("S", 65537)));
 print_r(unpack("S", pack("S", 0)));
 print_r(unpack("S", pack("S", -1000)));
 print_r(unpack("S", pack("S", -64434)));
 print_r(unpack("S", pack("S", -65535)));
-
-print_r(unpack("V", pack("V", 65534)));
-print_r(unpack("V", pack("V", 0)));
-print_r(unpack("V", pack("V", 2147483650)));
-print_r(unpack("V", pack("V", 4294967296)));
-print_r(unpack("V", pack("V", -2147483648)));
 
 print_r(unpack("a", pack("a", "hello world")));
 print_r(unpack("a*", pack("a*", "hello world")));
@@ -88,4 +110,6 @@ print_r(unpack("v", pack("v", 0)));
 print_r(unpack("v", pack("v", -1000)));
 print_r(unpack("v", pack("v", -64434)));
 print_r(unpack("v", pack("v", -65535)));
+  */
+
 ?>
