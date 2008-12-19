462e7000dd69a64b11d5007410e2740979f9001a
Shannon Weyrick <weyrick@mozek.us>
2008-12-19 14:35:28 +0000

    bump head to 2.9.9

--- runtime/php-runtime.scm
+++ runtime/php-runtime.scm
@@ -103,7 +103,7 @@
 
 (define *RAVEN-VERSION-MAJOR* 2)
 (define *RAVEN-VERSION-MINOR* 9)
-(define *RAVEN-VERSION-RELEASE* 8)
+(define *RAVEN-VERSION-RELEASE* 9)
 (define *RAVEN-VERSION-STRING* (format "~a.~a.~a~a"
 				       *RAVEN-VERSION-MAJOR*
 				       *RAVEN-VERSION-MINOR*
